90d5506c4d9253a99f2b2c06ff360ca38ea9111b
mpereyra <marianopereyra95@gmail.com>
2022-08-05 11:03:18 -0300

    bug

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Examen Primary',
   tagline: '',
   url: 'https://marianomat.github.io',
-  baseUrl: '/primary-exam/',
+  baseUrl: '/primary/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'marianomat', // Usually your GitHub org/user name.
-  projectName: 'primary-exam', // Usually your repo name.
+  projectName: 'primary', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
